d35e71a9fb1deae2d81a1ddbc31fac17033a77d5
Carlos Montealegre <carlosmm147@gmail.com>
2017-10-02 17:35:49 -0500

    Cribs en línea

---
+++ Cribs.js
@@ -0,0 +1,17 @@
+angular
+    .module('ngCribs')
+    .controller('cribsController', function($scope, cribsFactory) {
+
+        $scope.cribs;
+        
+        cribsFactory.getCribs().success(function(data){
+            $scope.cribs = data;
+
+        }).error(function(error){
+            console.log(error);
+
+        });
+
+
+        
+    });
